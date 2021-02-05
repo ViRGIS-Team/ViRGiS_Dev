160ab4807f5f08e4bf9648969e44491f2ab10bdb
Paul <runette@gmail.com>
2021-02-05 22:19:26 +0000

    update occulus

--- Assets/Oculus/VR/Scripts/Util/OVRSkeletonRenderer.cs
+++ Assets/Oculus/VR/Scripts/Util/OVRSkeletonRenderer.cs
@@ -1,12 +1,8 @@
 /************************************************************************************
 Copyright : Copyright (c) Facebook Technologies, LLC and its affiliates. All rights reserved.
 
-Licensed under the Oculus Utilities SDK License Version 1.31 (the "License"); you may not use
-the Utilities SDK except in compliance with the License, which is provided at the time of installation
-or download, or which otherwise accompanies this software in either electronic or hard copy form.
-
-You may obtain a copy of the License at
-https://developer.oculus.com/licenses/utilities-1.31
+Your use of this SDK or tool is subject to the Oculus SDK License Agreement, available at
+https://developer.oculus.com/licenses/oculussdk/
 
 Unless required by applicable law or agreed to in writing, the Utilities SDK distributed
 under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
@@ -164,7 +160,7 @@ public class OVRSkeletonRenderer : MonoBehaviour
 			SystemGestureMaterial = systemGestureMat;
 
 			BoneCapsule = boneCapsule;
-			
+
 			CapsuleGO = GameObject.CreatePrimitive(PrimitiveType.Capsule);
 			CapsuleCollider collider = CapsuleGO.GetComponent<CapsuleCollider>();
 			Destroy(collider);
@@ -191,7 +187,7 @@ public class OVRSkeletonRenderer : MonoBehaviour
 					CapsuleGO.SetActive(shouldRender);
 				}
 			}
-			
+
 			CapsuleGO.transform.rotation = BoneCapsule.CapsuleCollider.transform.rotation * _capsuleRotationOffset;
 			CapsuleGO.transform.position = BoneCapsule.CapsuleCollider.transform.TransformPoint(BoneCapsule.CapsuleCollider.center);
 			CapsuleGO.transform.localScale = capsuleScale * scale;
@@ -231,7 +227,20 @@ public class OVRSkeletonRenderer : MonoBehaviour
 			return;
 		}
 
-		Initialize();
+		if (ShouldInitialize())
+		{
+			Initialize();
+		}
+	}
+
+	private bool ShouldInitialize()
+	{
+		if (IsInitialized)
+		{
+			return false;
+		}
+
+		return _ovrSkeleton.IsInitialized;
 	}
 
 	private void Initialize()
@@ -297,6 +306,13 @@ public class OVRSkeletonRenderer : MonoBehaviour
 
 	public void Update()
 	{
+#if UNITY_EDITOR
+		if (ShouldInitialize())
+		{
+			Initialize();
+		}
+#endif
+
 		IsDataValid = false;
 		IsDataHighConfidence = false;
 		ShouldUseSystemGestureMaterial = false;
@@ -331,15 +347,6 @@ public class OVRSkeletonRenderer : MonoBehaviour
 				_capsuleVisualizations[i].Update(_scale, shouldRender, ShouldUseSystemGestureMaterial, _confidenceBehavior, _systemGestureBehavior);
 			}
 		}
-#if UNITY_EDITOR
-		else
-		{
-			if (OVRInput.IsControllerConnected(OVRInput.Controller.Hands) && !IsInitialized)
-			{
-				Initialize();
-			}
-		}
-#endif
 	}
 
 	private void OnDestroy()
